b3debe133434a69e7966b421bc7da36f9296c4db
Lisbeth Calcina <jliscayo@gmail.com>
2022-09-20 20:58:52 -0300

    ejercicio2

---
+++ assets/js/ejercicio2.js
@@ -0,0 +1,45 @@
+function operaciones () {
+    //suma 
+
+    var n1= parseInt(prompt("ingrese un número"));
+    var n2 = parseInt(prompt("ingresa otro número"));
+
+    var suma = n1 + n2;
+
+    
+    //resta
+
+    var n1= parseInt(prompt("Ingrese un número"));
+    var n2 = parseInt(prompt("ingresa otro número"));
+
+    var resta= n1 - n2;
+
+    
+
+
+    //multiplicacion
+
+    var n1= parseInt(prompt("Ingrese un número"));
+    var n2 = parseInt(prompt("ingresa otro número"));
+
+    var multi= n1 * n2;
+
+    
+
+    //disivion
+
+    var n1= parseInt(prompt("Ingrese un número"));
+    var n2 = parseInt(prompt("ingresa otro número"));
+
+    var div= n1 / n2;
+
+
+    alert("El valor de la suma es : "+ suma);
+    alert("El valor de la resta es : "+ resta);
+    alert("El valor de la multiplicación es: "+multi);
+    alert("El valor de la división es: "+div);
+
+    
+}
+
+operaciones();
